c2dd0b19cbced3de8a01ae1ce418c88fd59ebe25
George Mathew <georgegmathew12@gmail.com>
2026-05-22 13:36:49 -0500

    Use TWO_DIGIT_CENTURY_START=1925 via project pre_hook (no profile config needed)

--- dbt/av_employees/macros/parse_date_yy.sql
+++ dbt/av_employees/macros/parse_date_yy.sql
@@ -1,7 +1,3 @@
 {% macro parse_date_yy(col) %}
-    case
-        when year(try_to_date({{ col }}, 'MM/DD/YY')) >= 2025
-            then dateadd(year, -100, try_to_date({{ col }}, 'MM/DD/YY'))
-        else try_to_date({{ col }}, 'MM/DD/YY')
-    end
+    try_to_date({{ col }}, 'MM/DD/YY')
 {% endmacro %}
